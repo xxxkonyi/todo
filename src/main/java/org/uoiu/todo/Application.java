c79f3ebadbcde7149d60c1e953ed93b653166d8c
悾 <xxxkonyi@gmail.com>
2016-07-26 10:00:24 +0800

    增加docker配置

--- src/main/java/org/uoiu/todo/Application.java
+++ src/main/java/org/uoiu/todo/Application.java
@@ -1,32 +1,31 @@
-package io.daocloud;
+package org.uoiu.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.StandardEnvironment;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.text.MessageFormat;
 
 @SpringBootApplication
-@Controller
-public class TodoApplication {
+@RestController
+public class Application {
 
   @Autowired
   StandardEnvironment environment;
 
   @RequestMapping("/")
-  public void index(HttpServletResponse response) throws IOException {
-    response.getWriter().println(MessageFormat.format("Server {0} {1} is live!",
+  public String index(HttpServletResponse response) {
+    return MessageFormat.format("Server {0} {1} is live!",
       environment.getSystemEnvironment().getOrDefault("HOSTNAME", "windows"),
-      environment.getSystemProperties().get("PID")));
+      environment.getSystemProperties().get("PID"));
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(TodoApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
