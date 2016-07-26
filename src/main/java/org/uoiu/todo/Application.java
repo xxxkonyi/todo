e73fdec5cdfef7fb9a2a41cb07da824c742b58fa
悾 <xxxkonyi@gmail.com>
2016-07-26 16:41:29 +0800

    ratpack 集成

--- src/main/java/org/uoiu/todo/Application.java
+++ src/main/java/org/uoiu/todo/Application.java
@@ -1,31 +1,38 @@
 package org.uoiu.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.StandardEnvironment;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.text.MessageFormat;
+import ratpack.server.RatpackServer;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class Application {
 
   @Autowired
   StandardEnvironment environment;
 
-  @RequestMapping("/")
-  public String index(HttpServletResponse response) {
-    return MessageFormat.format("Server {0} {1} is live!",
-      environment.getSystemEnvironment().getOrDefault("HOSTNAME", "windows"),
-      environment.getSystemProperties().get("PID"));
-  }
+//  @RequestMapping("/")
+//  public String index(HttpServletResponse response) {
+//    return MessageFormat.format("Server {0} {1} is live!",
+//      environment.getSystemEnvironment().getOrDefault("HOSTNAME", "windows"),
+//      environment.getSystemProperties().get("PID"));
+//  }
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+//  @Bean
+//  public Handler handler() {
+//    return context -> context.render("Hello World");
+//  }
+
+  public static void main(String[] args) throws Exception {
+//    SpringApplication.run(Application.class, args);
+
+    RatpackServer.start(serverSpec -> serverSpec
+//      .registry(Spring.spring(Application.class))
+        .handlers(chain -> chain
+          .get(ctx -> ctx.getResponse().send("Hello, World!"))
+        )
+    );
   }
 
 }
